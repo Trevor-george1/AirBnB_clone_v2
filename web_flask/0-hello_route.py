0ec56b5d66f05fcae1a8e69e2c66f7f35f5e2ca1
Trevor <Trevormahinda@gmail.com>
2024-05-17 11:02:21 +0300

    Hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# scrpt that starts a FLAsk web application
+"""scrpt that starts a FLAsk web application"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Routing to root"""
     return "Hello HBNB!"
 
 
