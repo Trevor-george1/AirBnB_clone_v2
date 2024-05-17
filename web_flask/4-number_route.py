e4248ff9539f70bef303dc386a96dc729ddd3d1c
Trevor <Trevormahinda@gmail.com>
2024-05-17 11:39:45 +0300

    Von

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,7 +31,7 @@ def python_text(text="is cool"):
     return f"Python {escape(text)}"
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_display(n):
     return "{} is a number".format(n)
 
