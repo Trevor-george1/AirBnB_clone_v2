c3e2b5172ebada5ceb15664863ad7a825d435fac
Trevor <Trevormahinda@gmail.com>
2024-05-19 14:23:35 +0300

    add

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,7 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.update_at = datetime.utcnow()
 
         else:
             for key, value in kwargs.items():
